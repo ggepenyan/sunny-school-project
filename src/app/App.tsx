4818b8213bc6f70f10adf4c1c6d6ff38a8784225
Voskan Voskanyan <voskanvoskanyan@yandex.com>
2024-04-03 09:47:17 +0400

    first commit

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import styles from "./app.module.css";
+import { Button } from "@/components/ui/button";
+
+const App = () => {
+  return (
+    <div className="text-3xl font-bold underline text-red-500">
+      Appppppp 111117777
+      <Button>Click me</Button>
+    </div>
+  );
+};
+
+export default App;
